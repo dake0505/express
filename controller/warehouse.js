9dff7aa74ac938967453a4b594ed0b27c59463c9
dake0505 <dakejiang123@163.com>
2022-04-23 16:23:56 +0800

    update commodity update/delete

--- controller/warehouse.js
+++ controller/warehouse.js
@@ -4,7 +4,9 @@ const BaseRes = require('../util/baseRes')
 // 仓库商品列表
 exports.queryWarehouseCommodityList = async (req, res, next) => {
   try {
-    const { limit = 20, offset = 0 } = req.query
+    const { pageNumber = 1, pageSize = 10 } = req.query
+    const limit = pageSize
+    const offset = (pageNumber - 1) * pageSize
     const filter = {
       email: req.user.email
     }
@@ -27,6 +29,7 @@ exports.createWarehouseCommodity = async (req, res, next) => {
   try {
     const commodity = new Warehouse(req.body)
     commodity.createdBy = req.user.email
+    commodity.createAt = new Date()
     await commodity.save()
     res.status(200).json(BaseRes.success(commodity))
   } catch (error) {
@@ -37,7 +40,15 @@ exports.createWarehouseCommodity = async (req, res, next) => {
 // 修改仓库商品信息
 exports.updateWarehouseCommodity = async (req, res, next) => {
   try {
-    res.status(200).json(BaseRes.success('修改商品'))
+    const commodity = new Warehouse(req.body)
+    commodity.updatedBy = req.user.email
+    commodity.updateAt = new Date()
+    const updateItem = await Warehouse.findByIdAndUpdate(
+      req.body._id,
+      {$set: { ...commodity }}
+    )
+    console.log(updateItem)
+    res.status(200).json(BaseRes.success(updateItem))
   } catch (error) {
     next(error)
   }
@@ -46,7 +57,12 @@ exports.updateWarehouseCommodity = async (req, res, next) => {
 // 删除仓库商品
 exports.deleteWarehouseCommodity = async (req, res, next) => {
   try {
-    res.status(200).json(BaseRes.success('删除商品'))
+    const deleteItem = await Warehouse.findByIdAndDelete(req.query.id)
+    if (deleteItem) {
+      res.status(200).json(BaseRes.success('删除成功'))
+    } else {
+      res.status(200).json(BaseRes.fail('商品不存在'))
+    }
   } catch (error) {
     next(error)
   }
