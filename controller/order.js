d01cdb0a9c7418a1fb034fb472854ae62cda4c3b
dake0505 <dakejiang123@163.com>
2022-05-05 22:56:04 +0800

    update order api

--- controller/order.js
+++ controller/order.js
@@ -3,7 +3,11 @@ const BaseRes = require('../util/baseRes')
 
 exports.createOrder = async(req, res, next) => {
   try {
-    res.status(200).json(BaseRes.success('create order'))
+    const order = new Order(req.body)
+    order.createdBy = req.user.email
+    order.createdAt = new Date()
+    await order.save()
+    res.status(200).json(BaseRes.success(order))
   } catch (error) {
     next(error)
   }
@@ -11,7 +15,14 @@ exports.createOrder = async(req, res, next) => {
 
 exports.updateOrder = async(req, res, next) => {
   try {
-    res.status(200).json(BaseRes.success('update order'))
+    const order = new Order(req.body)
+    order.updatedBy = req.user.email
+    order.updatedAt = new Date()
+    const updateItem = await Order.findByIdAndUpdate(
+      req.body._id,
+      {$set: {...order}}
+    )
+    res.status(200).json(BaseRes.success(updateItem))
   } catch (error) {
     next(error)
   }
@@ -19,7 +30,12 @@ exports.updateOrder = async(req, res, next) => {
 
 exports.deleteOrder = async(req, res, next) => {
   try {
-    res.status(200).json(BaseRes.success('delete order'))
+    const deleteItem = await Order.findByIdAndDelete(req.query.id)
+    if (deleteItem) {
+      res.status(200).json(BaseRes.success('删除成功'))
+    } else {
+      res.status(200).json(BaseRes.fail('商品不存在'))
+    }
   } catch (error) {
     next(error)
   }
@@ -27,7 +43,21 @@ exports.deleteOrder = async(req, res, next) => {
 
 exports.queryOrder = async(req, res, next) => {
   try {
-    res.status(200).json(BaseRes.success('query order'))
+    const { pageNumber = 1, pageSize = 10} = req.query
+    const limit = pageSize
+    const offset = (pageNumber - 1) * pageSize
+    const filter = {
+      email: req.user.email
+    }
+    const orderList = await Order
+      .find(filter)
+      .skip(offset)
+      .limit(limit)
+    const count = await Warehouse.countDocuments({createdBy: req.user.email})
+    res.status(200).json(BaseRes.success({
+      list: orderList,
+      count
+    }))
   } catch (error) {
     next(error)
   }
